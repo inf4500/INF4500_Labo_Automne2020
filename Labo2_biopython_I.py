fe8d087261957991482c0ac381ba4488105fb5d6
Mathieu Lemieux <mathieu@mathieulemieux.com>
2020-09-16 07:56:09 -0400

    add biopython exercice I

---
+++ Labo2_biopython_I.py
@@ -0,0 +1,19 @@
+import sys
+from Bio import Entrez, SeqIO
+
+
+# Télécharger une séquence depuis GenBank
+def loadFromGenbank(noAccession):
+    Entrez.email = 'john.doe@domain.com'
+    handle       = Entrez.efetch(db="nucleotide", id=noAccession, rettype="gb")
+    rec          = SeqIO.read(handle, "genbank")
+    print (rec.format("gb"))   # On affiche le contenu
+    print (dir(rec))           # On affiche la liste des méthodes disponibles pour l'objet SeqIO
+
+    fileName = noAccession + ".gb"
+    SeqIO.write(rec, fileName, "gb") # Enregistrer la fiche localement
+
+#= ligne de commande
+if len(sys.argv) == 2:
+    loadFromGenbank(sys.argv[1]) # Attention, ici on passe le numéro d'accession via la console en appelant le programme.
+                                 # Vous pouvez essayer avec le numéro d'accession 'AB123456'. Ex. python AB123456
